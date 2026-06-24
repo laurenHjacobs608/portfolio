4565aecc3f63d2c32702d33a3931216620f7de69
laurenHjacobs608 <laurenjacobs608@gmail.com>
2026-06-23 19:28:29 -0600

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>
 );
